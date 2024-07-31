6e7897cae05e2a56abd12e0b331afb2cf8ae389d
Lovelyavimuwal <108040500+Lovelyavimuwal@users.noreply.github.com>
2024-08-01 00:22:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,4 +1,4 @@
-# Don't Remove Credit Tg - @VJ_Botz
+# Don't Remove Credit Tg - @snap_leaks_2024
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
@@ -7,10 +7,10 @@
 import os
 from os import environ
 
-API_ID       = int(environ.get("API_ID", ""))
-API_HASH     = environ.get("API_HASH", "")
+API_ID       = int(environ.get("API_ID", "18779212"))
+API_HASH     = environ.get("API_HASH", "1f6b28d46b9851b73c80553f0d2f5ea5")
 BOT_TOKEN    = environ.get("BOT_TOKEN", "")
-DATABASE_URI = environ.get("DATABASE_URI", "")
-LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", ""))
-ADMIN        = int(environ.get("ADMIN", ""))
-CHANNEL      = environ.get("CHANNEL", "@VJ_Botz")
+DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://goketyge:sh012W8rxeE6kWbh@cluster0.ssudbsk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1001658743981"))
+ADMIN        = int(environ.get("ADMIN", "6143551688"))
+CHANNEL      = environ.get("CHANNEL", "@snap_leaks_2024")
