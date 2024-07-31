412718228c8e34211cd643af4144970be89f7775
Lovelyavimuwal <108040500+Lovelyavimuwal@users.noreply.github.com>
2024-08-01 00:47:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -10,7 +10,7 @@ from os import environ
 API_ID       = int(environ.get("API_ID", "18779212"))
 API_HASH     = environ.get("API_HASH", "1f6b28d46b9851b73c80553f0d2f5ea5")
 BOT_TOKEN    = environ.get("BOT_TOKEN", "")
-DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://goketyge:sh012W8rxeE6kWbh@cluster0.ssudbsk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get("DATABASE_URI", "")
 LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1001658743981"))
 ADMIN        = int(environ.get("ADMIN", "6143551688"))
 CHANNEL      = environ.get("CHANNEL", "@YouTube_Sutra_Coolmitra_Abbyvira")
