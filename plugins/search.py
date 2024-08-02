a5d5f5100c4305ce14fe3422d60c8b5ebe7cfabc
Lovelyavimuwal <108040500+Lovelyavimuwal@users.noreply.github.com>
2024-08-02 19:36:31 +0530

    Update search.py

--- plugins/search.py
+++ plugins/search.py
@@ -25,121 +25,79 @@ async def delete_after_delay(message: Message, delay):
     except:
         pass
 
-# @Client.on_message(filters.text & filters.group & filters.incoming & ~filters.command(["verify", "connect", "id"]))
-# async def search(bot, message):
-#     vj = database.find_one({"chat_id": ADMIN})
-#     if vj == None:
-#         return await message.reply("**Contact Admin Then Say To Login In Bot.**")
-#     User = Client("post_search", session_string=vj['session'], api_hash=API_HASH, api_id=API_ID)
-#     await User.connect()
-#     f_sub = await force_sub(bot, message)
-#     if f_sub==False:
-#        return     
-#     channels = (await get_group(message.chat.id))["channels"]
-#     if bool(channels)==False:
-#        return     
-#     if message.text.startswith("/"):
-#        return    
-#     query   = message.text 
-#     head    = f"<u>⭕ Here is the results {message.from_user.mention} 👇\n\n💢 Powered By </u> <b><I>@snap_leaks_2024 ❗</I></b>\n\n"
-#     results = ""
-#     try:
-#        for channel in channels:
-#            async for msg in User.search_messages(chat_id=channel, query=query):
-#                name = (msg.text or msg.caption).split("\n")[0]
-#                if name in results:
-#                   continue 
-#                results += f"<b><I>♻️ {name}\n🔗 {msg.link}</I></b>\n\n"                                                      
-#        if bool(results)==False:
-#           movies = await search_imdb(query)
-#           buttons = []
-#           for movie in movies: 
-#               buttons.append([InlineKeyboardButton(movie['title'], callback_data=f"recheck_{movie['id']}")])
-#           msg = await message.reply_photo(photo="https://graph.org/file/c361a803c7b70fc50d435.jpg",
-#                                           caption="<b><I>🔻 I Couldn't find anything related to Your Query😕.\n🔺 Did you mean any of these?</I></b>", 
-#                                           reply_markup=InlineKeyboardMarkup(buttons))
-#        else:
-#           await send_message_in_chunks(bot, message.chat.id, head+results)
-#     except:
-#        pass
-       
-
-
-# @Client.on_callback_query(filters.regex(r"^recheck"))
-# async def recheck(bot, update):
-#     vj = database.find_one({"chat_id": ADMIN})
-#     User = Client("post_search", session_string=vj['session'], api_hash=API_HASH, api_id=API_ID)
-#     if vj == None:
-#         return await update.message.edit("**Contact Admin Then Say To Login In Bot.**")
-#     await User.connect()
-#     clicked = update.from_user.id
-#     try:      
-#        typed = update.message.reply_to_message.from_user.id
-#     except:
-#        return await update.message.delete(2)       
-#     if clicked != typed:
-#        return await update.answer("That's not for you! 👀", show_alert=True)
-
-#     m=await update.message.edit("**Searching..💥**")
-#     id      = update.data.split("_")[-1]
-#     query   = await search_imdb(id)
-#     channels = (await get_group(update.message.chat.id))["channels"]
-#     head    = "<u>⭕ I Have Searched Movie With Wrong Spelling But Take care next time 👇\n\n💢 Powered By </u> <b><I>@snap_leaks_2024 ❗</I></b>\n\n"
-#     results = ""
-#     try:
-#        for channel in channels:
-#            async for msg in User.search_messages(chat_id=channel, query=query):
-#                name = (msg.text or msg.caption).split("\n")[0]
-#                if name in results:
-#                   continue 
-#                results += f"<b><I>♻️🍿 {name}</I></b>\n\n🔗 {msg.link}</I></b>\n\n"
-#        if bool(results)==False:          
-#           return await update.message.edit("🔺 Still no results found! Please Request To Group Admin 🔻", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🎯 Request To Admin 🎯", callback_data=f"request_{id}")]]))
-#        await send_message_in_chunks(bot, update.message.chat.id, head+results)
-#     except Exception as e:
-#        await update.message.edit(f"❌ Error: `{e}`")
 @Client.on_message(filters.text & filters.group & filters.incoming & ~filters.command(["verify", "connect", "id"]))
 async def search(bot, message):
     vj = database.find_one({"chat_id": ADMIN})
-    if vj is None:
+    if vj == None:
         return await message.reply("**Contact Admin Then Say To Login In Bot.**")
     User = Client("post_search", session_string=vj['session'], api_hash=API_HASH, api_id=API_ID)
     await User.connect()
     f_sub = await force_sub(bot, message)
-    if not f_sub:
-        return     
+    if f_sub==False:
+       return     
     channels = (await get_group(message.chat.id))["channels"]
-    if not channels:
-        return     
+    if bool(channels)==False:
+       return     
     if message.text.startswith("/"):
-        return    
-    query = message.text 
-    results_found = False
+       return    
+    query   = message.text 
+    head    = f"<u>⭕ Here is the results {message.from_user.mention} 👇\n\n💢 Powered By </u> <b><I>@snap_leaks_2024 ❗</I></b>\n\n"
+    results = ""
     try:
-        for channel in channels:
-            async for msg in User.search_messages(chat_id=channel, query=query):
-                if msg.text:
-                    await send_message_in_chunks(bot, message.chat.id, msg.text)
-                elif msg.photo:
-                    await bot.send_photo(chat_id=message.chat.id, photo=msg.photo.file_id, caption=msg.caption or "")
-                elif msg.video:
-                    await bot.send_video(chat_id=message.chat.id, video=msg.video.file_id, caption=msg.caption or "")
-                elif msg.document:
-                    await bot.send_document(chat_id=message.chat.id, document=msg.document.file_id, caption=msg.caption or "")
-                results_found = True
-        
-        if not results_found:
-            movies = await search_imdb(query)
-            buttons = []
-            for movie in movies: 
-                buttons.append([InlineKeyboardButton(movie['title'], callback_data=f"recheck_{movie['id']}")])
-            await message.reply_photo(
-                photo="https://graph.org/file/c361a803c7b70fc50d435.jpg",
-                caption="<b><I>🔻 I Couldn't find anything related to Your Query😕.\n🔺 Did you mean any of these?</I></b>", 
-                reply_markup=InlineKeyboardMarkup(buttons)
-            )
+       for channel in channels:
+           async for msg in User.search_messages(chat_id=channel, query=query):
+               name = (msg.text or msg.caption).split("\n")[0]
+               if name in results:
+                  continue 
+               results += f"<b><I>♻️ {name}\n🔗 {msg.link}</I></b>\n\n"                                                      
+       if bool(results)==False:
+          movies = await search_imdb(query)
+          buttons = []
+          for movie in movies: 
+              buttons.append([InlineKeyboardButton(movie['title'], callback_data=f"recheck_{movie['id']}")])
+          msg = await message.reply_photo(photo="https://graph.org/file/c361a803c7b70fc50d435.jpg",
+                                          caption="<b><I>🔻 I Couldn't find anything related to Your Query😕.\n🔺 Did you mean any of these?</I></b>", 
+                                          reply_markup=InlineKeyboardMarkup(buttons))
+       else:
+          await send_message_in_chunks(bot, message.chat.id, head+results)
+    except:
+       pass
+       
+
+
+@Client.on_callback_query(filters.regex(r"^recheck"))
+async def recheck(bot, update):
+    vj = database.find_one({"chat_id": ADMIN})
+    User = Client("post_search", session_string=vj['session'], api_hash=API_HASH, api_id=API_ID)
+    if vj == None:
+        return await update.message.edit("**Contact Admin Then Say To Login In Bot.**")
+    await User.connect()
+    clicked = update.from_user.id
+    try:      
+       typed = update.message.reply_to_message.from_user.id
+    except:
+       return await update.message.delete(2)       
+    if clicked != typed:
+       return await update.answer("That's not for you! 👀", show_alert=True)
+
+    m=await update.message.edit("**Searching..💥**")
+    id      = update.data.split("_")[-1]
+    query   = await search_imdb(id)
+    channels = (await get_group(update.message.chat.id))["channels"]
+    head    = "<u>⭕ I Have Searched Movie With Wrong Spelling But Take care next time 👇\n\n💢 Powered By </u> <b><I>@snap_leaks_2024 ❗</I></b>\n\n"
+    results = ""
+    try:
+       for channel in channels:
+           async for msg in User.search_messages(chat_id=channel, query=query):
+               name = (msg.text or msg.caption).split("\n")[0]
+               if name in results:
+                  continue 
+               results += f"<b><I>♻️🍿 {name}</I></b>\n\n🔗 {msg.link}</I></b>\n\n"
+       if bool(results)==False:          
+          return await update.message.edit("🔺 Still no results found! Please Request To Group Admin 🔻", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🎯 Request To Admin 🎯", callback_data=f"request_{id}")]]))
+       await send_message_in_chunks(bot, update.message.chat.id, head+results)
     except Exception as e:
-        print(f"Error: {e}")
+       await update.message.edit(f"❌ Error: `{e}`")
 
 
 @Client.on_callback_query(filters.regex(r"^request"))
